149c332325b1a43928549417e35aca2060457816
Hendra Afrizal <hendraafrizal30@gmail.com>
2022-01-06 11:37:32 +0700

    data3000

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -163,7 +163,7 @@ class DatabaseSeeder extends Seeder
             'jenis' => 'Tanah Berbatu'
         ]);
 
-        DataSurvey::factory(30)->create();
+        DataSurvey::factory(3000)->create();
         LampiranFoto::factory(30)->create();
         Fasos::factory(30)->create();
 
