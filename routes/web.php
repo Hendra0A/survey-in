0c70757419409a1e7f76261c8056f3e536a9f8fd
riskypaditya <donloadfilem@gmail.com>
2022-01-11 15:06:46 +0700

    melengkapi routes

--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,9 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('/profile', [SurveyorController::class, 'show']);
         Route::get('/edit-profile/surveyor', [SurveyorController::class, 'update']);
         Route::get('/data-survei', [SurveyorController::class, 'dataSurvei']);
+        Route::get('/pengaturan', [SurveyorController::class, 'pengaturan']);
+        Route::get('/pengaturan/edit-password', [SurveyorController::class, 'ubahPassword']);
+        Route::post('/pengaturan/edit-password', [SurveyorController::class, 'updatePassword']);
     });
     Route::post('/logout', [AccessController::class, 'logout']);
 });
@@ -66,3 +69,33 @@ Route::group(['middleware' => 'guest'], function () {
     Route::get('/', [AccessController::class, 'index'])->name('login');
     Route::post('/', [AccessController::class, 'authenticate']);
 });
+
+
+// Belom berubah
+// Profile Admin
+Route::get('/profile/{User:id}', [AdminController::class, 'profile']);
+Route::get('/profile', [AdminController::class, 'profile']);
+Route::get('/profile/edit-profile/admin', [AdminController::class, 'profileEdit']);
+Route::patch('/profile/edit-profile/admin', [AdminController::class, 'profileUpdate']);
+
+// Halaman Pengaturan Admin
+Route::get('/pengaturan', [AdminController::class, 'pengaturan']);
+Route::get('/pengaturan/edit-data-survey', [AdminController::class, 'editDataSurvey']);
+Route::put('/pengaturan/edit-data-survey', [AdminController::class, 'editData']);
+Route::post('/pengaturan/edit-data-survey/{model}/tambah', [AdminController::class, 'createData']);
+Route::put('/pengaturan/edit-data-survey/hapus/', [AdminController::class, 'destroy']);
+Route::get('/pengaturan/ubah-password', [AdminController::class, 'ubahPassword']);
+Route::post('/pengaturan/ubah-password', [AdminController::class, 'updatePassword']);
+
+// Halaman Data Survei
+Route::get('/data-survei', [DataSurveyController::class, 'index'])->name('data-survei');
+Route::get('/data-survei/{id}', [DataSurveyController::class, 'detail']);
+Route::put('/data-survei', [DataSurveyController::class, 'destroy']);
+Route::get('/data-survei/print/resume/{id}', [DataSurveyController::class, 'printResume']);
+Route::get('/data-survei/resume/{id}', [DataSurveyController::class, 'previewResume']);
+Route::get('/data-survei/print/{id}', [DataSurveyController::class, 'printPDF']);
+// Route::post('data-survei', [AdminController::class, 'getData'])->name('get-data');
+
+
+
+// Halaman User
