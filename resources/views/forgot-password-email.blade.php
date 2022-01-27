81f989a6854aa52a0a786ef3e6fd5cc19fee81e9
Haris Wijayanto <hrswjynto@gmail.com>
2022-01-27 11:15:01 +0700

    lkasjsdfklsaj

--- resources/views/forgot-password-email.blade.php
+++ resources/views/forgot-password-email.blade.php
@@ -43,18 +43,17 @@
 								</tr>
 								<tr>
 									<td style="padding:0 35px;">
-										<h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You have
-											requested to reset your password</h1>
+										<h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Hai
+											{{ $name }}</h1>
 										<span
 											style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
 										<p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
-											We cannot simply send you your old password. A unique link to reset your
-											password has been generated for you. To reset your password, click the
-											following link and follow the instructions.
-										</p>
+											Password Survey kamu dapat di atur ulang dengan mengklik
+											tombol dibawah. Jika kamu merasa tidak meminta pengaturan
+											ulang password, kamu dapat mengabaikan email ini. </p>
 										<a href="https://survey.idekite.id/reset-password/{{ $token }};"
-											style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
-											Password</a>
+											style="background:#0d6efd;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:0.3rem;">Atur
+											Ulang Password</a>
 									</td>
 								</tr>
 								<tr>
